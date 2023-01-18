5959ff6a7223dfd3d9d46b3abff45b1bc5e202dc
BenYahMin <112189097+BenYahMin@users.noreply.github.com>
2023-01-18 16:13:24 +0300

    Add files via upload

---
+++ errorMSG.bat
@@ -0,0 +1,26 @@
+@echo off
+
+cls
+
+echo enter a password of your choice
+
+set /p pass=
+
+call msg
+
+:msg
+
+@if (true == false) @end /*!
+
+@echo off
+
+mshta "about:
+
+" %*
+goto :EOF */
+
+alert("Hello, world!");
+
+pause
+
+pause
